70f12d766c990bb7f91f70dbd240ebf5d98196bc
sateesh thavitireddy <sateesh.thavitireddy@gmail.com>
2026-01-08 13:58:39 +0000

    adding files

--- mongodb.sh
+++ mongodb.sh
@@ -6,40 +6,41 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shellscript"
-SCRIPT_NAME="$(echo $0 | cut -d "." -f1 )"
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 mkdir -p $LOGS_FOLDER
-echo -e "$R Starting the script execution $N" | tee -a $LOG_FILE
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
-    echo -e "$R you cannot execute this script as root user $N" | tee -a $LOG_FILE
-    exit 1
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-VALIDATE(){
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "$R $2 failed $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
-    else 
-        echo -e "$G $2 is completed successfully $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Copying Mongodb repo file"
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo"
 
 dnf install mongodb-org -y &>>$LOG_FILE
-VALIDATE $? "Installing Mongodb"
+VALIDATE $? "Installing MongoDB"
 
 systemctl enable mongod &>>$LOG_FILE
-VALIDATE $? "Enabling Mongodb service"
+VALIDATE $? "Enable MongoDB"
 
-systemctl start mongod  &>>$LOG_FILE
-VALIDATE $? "Starting Mongodb service"
+systemctl start mongod 
+VALIDATE $? "Start MongoDB"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf  &>>$LOG_FILE
-VALIDATE $? "Updating Mongodb bind IP"
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
 
-systemctl restart mongod &>>$LOG_FILE
-VALIDATE $? "Restarting Mongodb service"
+systemctl restart mongod
+VALIDATE $? "Restarted MongoDB"
