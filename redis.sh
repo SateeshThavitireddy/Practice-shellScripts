38ad95a4271e8491520b0d6cdd8b15c84b5d9166
sateesh thavitireddy <sateesh.thavitireddy@gmail.com>
2026-01-09 01:10:05 +0000

    adding files

---
+++ redis.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript"
+SCRIPT_NAME="$(echo $0 | cut -d "." -f1 )"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIRECTORY=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo -e "$G Starting Frontend Setup... $N" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run as root or use sudo. $N" | tee -a $LOG_FILE
+    exit 1
+fi
+
+ALREADY_INSTALLED(){
+    dnf module list $?
+    if [ $1 -ne 0 ]; then
+        echo -e "$Y $2 is not installed. Ready to install. $N" | tee -a $LOG_FILE
+        dnf install $2 -y
+        VALIDATE $? $2
+    else
+        echo -e "$G $2 is already installed. $N" | tee -a $LOG_FILE
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R Failed to install $2. Check the log file at $LOG_FILE for details. $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$G Successfully installed $2. $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling existing Redis module"
+
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Redis 7 module"
+
+dnf install redis -y &>>$LOG_FILE
+VALIDATE $? "Installing Redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf -e 's/protected-mode yes/protected-mode no/g' &>>$LOG_FILE
+VALIDATE $? "Configuring Redis to allow external connections"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling Redis service to start on boot"
+
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Starting Redis service"
+
+echo -e "$G Redis setup completed successfully. $N" | tee -a $LOG_FILE
